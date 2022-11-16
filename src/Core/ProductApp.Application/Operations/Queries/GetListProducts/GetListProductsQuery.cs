063799684d83545b994c8779226a3cb8a67ebc2b
Serdar Ergün <generalgord@gmail.com>
2022-11-16 13:35:40 +0300

    Added AutoMapper and simple CQRS patters to start

---
+++ src/Core/ProductApp.Application/Operations/Queries/GetListProducts/GetListProductsQuery.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+
+using ProductApp.Application.Common.DTO;
+using ProductApp.Application.Common.Repositories;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Operations.Queries.GetListProducts
+{
+    public class GetListProductsQuery : IRequest<IList<ProductViewDTO>>
+    {
+        public class GetListProductsQueryHandler : IRequestHandler<GetListProductsQuery, IList<ProductViewDTO>>
+        {
+            private readonly IProductRepository productRepository;
+
+            public GetListProductsQueryHandler(IProductRepository productRepository)
+            {
+                this.productRepository = productRepository;
+            }
+
+            public async Task<IList<ProductViewDTO>> Handle(GetListProductsQuery request, CancellationToken cancellationToken)
+            {
+                var list = await productRepository.GetAllAsync();
+
+                return new List<ProductViewDTO>() { };
+
+            }
+        }
+    }
+}
--- src/Core/ProductApp.Application/ServiceRegistration.cs
+++ src/Core/ProductApp.Application/ServiceRegistration.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using MediatR;
+
+using ProductApp.Application.Common.Repositories;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Microsoft.Extensions.DependencyInjection;
 public static class ServiceRegistration
 {
+    public static void AddApplicationServices(this IServiceCollection serviceDescriptors)
+    {
+        var assemblies = Assembly.GetExecutingAssembly();
+        serviceDescriptors.AddAutoMapper(assemblies);
+        serviceDescriptors.AddMediatR(assemblies);
+    }
 }
