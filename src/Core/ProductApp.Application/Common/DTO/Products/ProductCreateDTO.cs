2dbcde461518c2eed62c32981a1df45217118b87
Serdar Ergün <generalgord@gmail.com>
2022-11-17 17:20:10 +0300

    Created product post model

---
+++ src/Core/ProductApp.Application/Common/DTO/Products/ProductCreateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductApp.Application.Common.DTO.Products
+{
+    public class ProductCreateDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+    }
+}
